77b86d255ce10fe459d84ff425befaf727cca33e
Damian Montoya <dmontoya1600@gmail.com>
2021-06-17 20:16:04 -0500

    completed auth routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-
-// GET /api/set-token-cookie
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
 module.exports = router;
