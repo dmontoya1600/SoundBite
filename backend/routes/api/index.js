9bfa956e6caad287fc1a79b4f2c2541db41d1490
Damian Montoya <dmontoya1600@gmail.com>
2021-06-17 19:56:16 -0500

    completed phase 4 backend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 
 
